e6650574488c9f7dae3021a526db27f50e487400
Ryan Sole <solemanryan@icloud.com>
2023-01-17 16:50:03 -0600

    Completely revamped and changed specific code

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-// Navbar Menu Start
-const toggleMenuOpen = () => document.body.classList.toggle("open");
-// Navbar Menu End
+// NAV/HAMBURGER START
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
+// NAV/HAMBURGER END
